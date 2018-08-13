e594e9be05c9e7094bc10eb062f0ab0a49c98016
loadkpi <loadkpi@gmail.com>
2018-08-13 11:52:11 +0300

    prevent nil user agent

--- spec/crawler_detect_spec.rb
+++ spec/crawler_detect_spec.rb
@@ -10,8 +10,16 @@ RSpec.describe CrawlerDetect do
   end
 
   describe ".is_crawler?" do
-    subject { described_class.is_crawler?("something") }
+    subject { described_class.is_crawler?(user_agent) }
+
+    let(:user_agent) { "something" }
 
     it { is_expected.to be(true).or be(false) }
+
+    context "empty header" do
+      let(:user_agent) { nil }
+
+      it { is_expected.to be(true).or be(false) }
+    end
   end
 end
